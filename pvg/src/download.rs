8691a4fee73e671984c8867b745323f9f3b1e239
karin0 <karin0@gmx.com>
2025-11-22 22:28:08 +0900

    refactor(download): remove `DownloadGuard`

--- pvg/src/download.rs
+++ pvg/src/download.rs
@@ -9,30 +9,12 @@ use tokio::io::AsyncWriteExt;
 use tokio::sync::mpsc::UnboundedSender;
 use tokio::time::Instant;
 
-#[derive(Debug, Default)]
-struct DownloadGuard(bool);
-
-impl DownloadGuard {
-    fn release(&mut self) {
-        self.0 = true;
-    }
-}
-
-impl Drop for DownloadGuard {
-    fn drop(&mut self) {
-        if !self.0 {
-            bug!("unfinished download dropped");
-        }
-    }
-}
-
 #[derive(Debug)]
 pub struct DownloadingFile {
     path: PathBuf,
-    file: fs::File,
+    file: Option<fs::File>,
     time: Option<Instant>,
     size: usize,
-    guard: DownloadGuard,
 }
 
 impl DownloadingFile {
@@ -44,10 +26,9 @@ impl DownloadingFile {
             .await?;
         Ok(Self {
             path,
-            file,
+            file: Some(file),
             time: None,
             size: 0,
-            guard: DownloadGuard::default(),
         })
     }
 
@@ -56,12 +37,19 @@ impl DownloadingFile {
     }
 
     pub async fn write(&mut self, b: &Bytes) -> io::Result<()> {
-        self.file.write_all(b).await?;
+        let Some(ref mut file) = self.file else {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "write to closed DownloadingFile",
+            ));
+        };
+        file.write_all(b).await?;
         self.size += b.len();
         Ok(())
     }
 
-    pub async fn commit(self, path: &Path, size: Option<u64>) -> Result<u64> {
+    pub async fn commit(mut self, path: &Path, size: Option<u64>) -> Result<u64> {
+        drop(self.file.take());
         if let Some(expected) = size {
             if self.size != expected as usize {
                 let size = self.size;
@@ -72,9 +60,33 @@ impl DownloadingFile {
             debug!("unknown size, written {}", self.size);
         }
 
+        #[cfg(feature = "rename2")]
+        {
+            use log::Level::{Debug, Error};
+            use nix::fcntl::{AT_FDCWD, RenameFlags, renameat2};
+
+            let res = renameat2(
+                AT_FDCWD,
+                &self.path,
+                AT_FDCWD,
+                path,
+                RenameFlags::RENAME_NOREPLACE,
+            );
+            log!(
+                if res.is_ok() { Debug } else { Error },
+                "{} -> {}: renameat2: {:?}",
+                self.path.display(),
+                path.display(),
+                res
+            );
+
+            // Falling back to fs::rename if renameat2 fails.
+            if res.is_ok() {
+                return Ok(self.finalize());
+            }
+        };
+
         // Create a new file to ensure that the target does not exist.
-        // This is unnecessary if `renameat2` is available.
-        #[cfg(not(feature = "rename2"))]
         match fs::OpenOptions::new()
             .write(true)
             .create_new(true)
@@ -88,71 +100,61 @@ impl DownloadingFile {
             }
         }
 
-        Ok(self.do_commit(path).await?)
-    }
-
-    async fn do_commit(mut self, path: &Path) -> io::Result<u64> {
-        self.guard.release();
-        drop(self.file);
-
-        #[cfg(not(feature = "rename2"))]
         let res = fs::rename(&self.path, path).await;
-
-        #[cfg(feature = "rename2")]
-        let res = {
-            use nix::fcntl::{AT_FDCWD, RenameFlags, renameat2};
-
-            let res = renameat2(
-                AT_FDCWD,
-                &self.path,
-                AT_FDCWD,
-                path,
-                RenameFlags::RENAME_NOREPLACE,
-            );
-            debug!(
-                "{} -> {}: renameat2 {:?}",
-                self.path.display(),
-                path.display(),
-                res
-            );
-
-            res.map_err(|e| io::Error::from_raw_os_error(e as i32))
-        };
 
         if let Err(e) = res {
             critical!("{:?}: COMMIT FAILED: {}", self.path, e);
-            Self::do_rollback(&self.path, self.size).await;
-            Err(e)
+            self.rollback().await;
+            Err(e.into())
         } else {
-            if let Some(t) = self.time {
-                let t = t.elapsed().as_secs_f32();
-                let kib = self.size as f32 / 1024.;
-                debug!(
-                    "{}: committed {:.3} KiB in {:.3} secs ({:.3} KiB/s)",
-                    self.path.display(),
-                    kib,
-                    t,
-                    kib / t
-                );
-            } else {
-                debug!("{}: committed {} B", self.path.display(), self.size);
-            }
-            Ok(self.size as u64)
+            Ok(self.finalize())
         }
     }
 
-    pub async fn rollback(mut self) {
-        self.guard.release();
-        drop(self.file);
-        Self::do_rollback(&self.path, self.size).await;
+    fn finalize(self) -> u64 {
+        if let Some(t) = self.time {
+            let t = t.elapsed().as_secs_f32();
+            let kib = self.size as f32 / 1024.;
+            debug!(
+                "{}: committed {:.3} KiB in {:.3} secs ({:.3} KiB/s)",
+                self.path.display(),
+                kib,
+                t,
+                kib / t
+            );
+        } else {
+            debug!("{}: committed {} B", self.path.display(), self.size);
+        }
+        self.size as u64
     }
 
-    async fn do_rollback(path: &Path, size: usize) {
-        if let Err(e) = fs::remove_file(&path).await {
+    pub async fn rollback(mut self) {
+        drop(self.file.take());
+        Self::rollback_finalize(&self.path, self.size, fs::remove_file(&self.path).await);
+    }
+
+    fn rollback_finalize(path: &Path, size: usize, res: io::Result<()>) {
+        if let Err(e) = res {
             critical!("{:?}: ROLLBACK FAILED ({} bytes): {}", path, size, e);
         } else {
             warn!("{}: rolled back {size} bytes", path.display());
         }
+    }
+}
+
+impl Drop for DownloadingFile {
+    fn drop(&mut self) {
+        let Some(file) = self.file.take() else {
+            return;
+        };
+        bug!("{}: unfinalized DownloadingFile", self.path.display());
+        match file.try_into_std() {
+            Ok(file) => drop(file),
+            Err(e) => {
+                error!("{}: try_into_std failed on {:?}", self.path.display(), e);
+            }
+        }
+        Self::rollback_finalize(&self.path, self.size, std::fs::remove_file(&self.path));
     }
 }
 
